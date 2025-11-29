7f516079be5c646d4f86dd7e7ea265952d000c9c
Limelightwo <limelight2yt@gmail.com>
2025-11-29 17:29:26 +0000

    Another Initial commit
    blablabla

--- src/main/java/org/limelight/iceBoatRace/IceBoatRace.java
+++ src/main/java/org/limelight/iceBoatRace/IceBoatRace.java
@@ -1,10 +1,23 @@
 package org.limelight.iceBoatRace;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Vehicle;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+import java.util.Map;
+
 
 public final class IceBoatRace extends JavaPlugin implements Listener {
+
+    public static String eventStatus;
+    public static List<String> avalibleMaps;
+    public static String currentMap;
+    public static Map<String, Location> mapSpawnLocations;
+    public static Map<String,Integer[]> mapFinishLines;
 
 
     @Override
