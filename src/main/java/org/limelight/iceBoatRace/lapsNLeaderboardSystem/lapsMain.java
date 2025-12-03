60401ebd6bc78f92ac627dffd7a96a10444052a1
Kangawooo <kangawoooyt@gmail.com>
2025-12-03 23:47:03 +0000

    Update lapsMain.java

--- src/main/java/org/limelight/iceBoatRace/lapsNLeaderboardSystem/lapsMain.java
+++ src/main/java/org/limelight/iceBoatRace/lapsNLeaderboardSystem/lapsMain.java
@@ -1,4 +1,5 @@
 package org.limelight.iceBoatRace.lapsNLeaderboardSystem;
+import org.limelight.iceBoatRace.boatSystem.boatListener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
@@ -23,8 +24,10 @@ public class lapsMain implements Listener {
     HashMap<Player, Boolean> playerBehindLine = new HashMap<Player, Boolean>();
 
     private static NamespacedKey playerLap;
+    private static JavaPlugin plugin;
 
     public lapsMain(JavaPlugin plugin){
+        this.plugin = plugin;
         playerLap = new NamespacedKey(plugin,"playerLap");
     }
 
@@ -36,7 +39,7 @@ public class lapsMain implements Listener {
 
     @EventHandler @Deprecated
     public void VehicleMoveEvent(VehicleMoveEvent event) {
-        // Ensure vehicle is a boat, boat has a passenger
+        // Ensure vehicle is a boat
         if (!(event.getVehicle() instanceof Boat boat)) return;
 
         //Ensure the boat has a passenger
@@ -50,33 +53,37 @@ public class lapsMain implements Listener {
         Vector2f playerPos = new Vector2f((float) boat.getX(), (float) boat.getZ());
         boolean inBox = isBehindLine(playerPos, test_point_1, -(1/gradient)) && !isBehindLine(playerPos, test_point_2, -(1/gradient));
 
-        // If the boat is not within rectangle perpendicular to the lap line, then do not continue
-        if (!inBox) return;
-
 
         boolean behindLine = isBehindLine(playerPos, test_point_1, gradient);
 
-        // If the boat has its data in the playerBehindLine continue
-        if (playerBehindLine.get(player) != null) {
-            boolean oldBehindLine = playerBehindLine.get(player);
+        // If the boat is not within rectangle perpendicular to the lap line, then do not continue
+        if (inBox) {
+            // If the boat has its data in the playerBehindLine continue
+            if (playerBehindLine.get(player) != null) {
+                boolean oldBehindLine = playerBehindLine.get(player);
 
-            // If the boat used to be behind the lap line
-            if (oldBehindLine) {
+                // If the boat used to be behind the lap line
+                if (oldBehindLine) {
 
-                // If the boat is now in front of the lap line
-                if (!behindLine) {
-                    PersistentDataContainer pdc = boat.getPersistentDataContainer();
+                    // If the boat is now in front of the lap line
+                    if (!behindLine) {
+                        PersistentDataContainer pdc = player.getPersistentDataContainer();
 
-                    // If the boat does not yet have its lap saved in its PDC, set its lap to 1
-                    // Otherwise, get its lap from the playerBehind and increment it
-                    int currentLap = 1;
-                    if (pdc.get(playerLap, PersistentDataType.INTEGER) != null) {
-                        currentLap = pdc.get(playerLap, PersistentDataType.INTEGER) + 1;
+                        // If the player does not yet have their lap saved in their PDC, set the lap to 1
+                        // Otherwise, get their lap from the playerLap key and increment it
+                        int currentLap = 1;
+                        if (pdc.get(playerLap, PersistentDataType.INTEGER) != null) {
+                            currentLap = pdc.get(playerLap, PersistentDataType.INTEGER) + 1;
+                        }
+
+                        // Save players currentLap and announce new lap
+                        pdc.set(playerLap, PersistentDataType.INTEGER, currentLap);
+                        Bukkit.broadcastMessage(player.getName() + " Lap: " + currentLap);
                     }
-
-                    // Save players currentLap and announce new lap
-                    pdc.set(playerLap, PersistentDataType.INTEGER, currentLap);
-                    Bukkit.broadcastMessage(player.getName() + " Lap: " + currentLap);
+                } else if (behindLine) {
+                    boat.eject();
+                    boat.remove();
+                    Bukkit.getScheduler().runTaskLater(plugin, () -> boatListener.spawnRacer(player, boat.getLocation()), 10);
                 }
             }
         }
