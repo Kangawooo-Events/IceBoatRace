2826ed66a977511317d67c8a8792c0874d637a48
Limelightwo <limelight2yt@gmail.com>
2025-12-03 07:13:38 +0000

    Made small changes for ultra-optimization

--- src/main/java/org/limelight/iceBoatRace/lapsNLeaderboardSystem/lapsMain.java
+++ src/main/java/org/limelight/iceBoatRace/lapsNLeaderboardSystem/lapsMain.java
@@ -3,8 +3,8 @@ package org.limelight.iceBoatRace.lapsNLeaderboardSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Boat;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
@@ -14,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.joml.Vector2f;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class lapsMain implements Listener {
     Vector2f test_point_1 = new Vector2f(-2885.5f, 2188.5f);
@@ -30,39 +31,42 @@ public class lapsMain implements Listener {
     // FUNCTIONS
     public static boolean isBehindLine(Vector2f point1, Vector2f point2, float gradient) {
         // Check if point1 is behind the line that sits on point2 with the included gradient
-        if (point1.y < gradient * (point1.x - point2.x) + point2.y) {
-            return true;
-        }
-        return false;
+        return point1.y < gradient * (point1.x - point2.x) + point2.y;
     }
 
     @EventHandler @Deprecated
     public void VehicleMoveEvent(VehicleMoveEvent event) {
-        // Ensure vehicle is a boat, boat has a passenger and the passenger is a player
-        if (!(event.getVehicle() instanceof Boat)) { return; }
-        if ((event.getVehicle().getPassengers().isEmpty())) { return; }
-        if (!(event.getVehicle().getPassengers().getFirst() instanceof Player)) { return; }
+        // Ensure vehicle is a boat, boat has a passenger
+        if (!(event.getVehicle() instanceof Boat boat)) return;
 
-        Vehicle boat = event.getVehicle();
-        Player player = (Player) boat.getPassengers().getFirst();
+        //Ensure the boat has a passenger
+        List<Entity> passengers = boat.getPassengers();
+        if (passengers.isEmpty()) return;
+
+        //Ensure the driver is a player
+        if (!(passengers.getFirst() instanceof Player player)) return;
 
         float gradient = Math.abs(test_point_1.x - test_point_2.x) / Math.abs(test_point_1.y - test_point_2.y);
-        Vector2f playerPos = new Vector2f((float) player.getLocation().getX(), (float) player.getLocation().getZ());
+        Vector2f playerPos = new Vector2f((float) boat.getX(), (float) boat.getZ());
         boolean inBox = isBehindLine(playerPos, test_point_1, -(1/gradient)) && !isBehindLine(playerPos, test_point_2, -(1/gradient));
 
         // If the boat is not within rectangle perpendicular to the lap line, then do not continue
-        if (!inBox) { return; }
+        if (!inBox) return;
+
 
         boolean behindLine = isBehindLine(playerPos, test_point_1, gradient);
-        PersistentDataContainer pdc = boat.getPersistentDataContainer();
 
         // If the boat has its data in the playerBehindLine continue
         if (playerBehindLine.get(player) != null) {
             boolean oldBehindLine = playerBehindLine.get(player);
+
             // If the boat used to be behind the lap line
-            if (Boolean.TRUE.equals(oldBehindLine)) {
+            if (oldBehindLine) {
+
                 // If the boat is now in front of the lap line
                 if (!behindLine) {
+                    PersistentDataContainer pdc = boat.getPersistentDataContainer();
+
                     // If the boat does not yet have its lap saved in its PDC, set its lap to 1
                     // Otherwise, get its lap from the playerBehind and increment it
                     int currentLap = 1;
