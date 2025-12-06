2f252271f97386e99a134fff40bc62f9d8857abe
Limelightwo <limelight2yt@gmail.com>
2025-12-06 20:17:57 +0000

    BoatListner -> Fixed Enum Migration

--- src/main/java/org/limelight/iceBoatRace/boatSystem/BoatListener.java
+++ src/main/java/org/limelight/iceBoatRace/boatSystem/BoatListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.limelight.iceBoatRace.IceBoatRace;
 
 import static org.limelight.iceBoatRace.IceBoatRace.currentMap;
 import static org.limelight.iceBoatRace.IceBoatRace.eventStatus;
@@ -27,7 +28,7 @@ public class BoatListener implements Listener {
     //Doesn't allow the player to leave a boat while in game (might change this to never)
     @EventHandler
     public void VehicleLeaveEvent(VehicleExitEvent event){
-        if (!eventStatus.equals("off")) event.setCancelled(true);
+        if (!(eventStatus == IceBoatRace.EventStatus.OFF)) event.setCancelled(true);
     }
 
 
@@ -35,13 +36,13 @@ public class BoatListener implements Listener {
     //Doesn't allow the player to enter a boat while in game (might change this to never)
     @EventHandler
     public void VehicleEnterEvent(VehicleEnterEvent event){
-        if (eventStatus.equals("in_progress")) event.setCancelled(true);
+        if (!(eventStatus == IceBoatRace.EventStatus.IN_PROGRESS || eventStatus == IceBoatRace.EventStatus.VOTING)) event.setCancelled(true);
     }
 
     //Removes the player from a race if they leave during a race
     @EventHandler
     public void PlayerLeave(PlayerQuitEvent event){
-        if (!eventStatus.equals("off")){
+        if (!(eventStatus == IceBoatRace.EventStatus.OFF)){
             Player player = event.getPlayer();
             currentMap.players.remove(player);
             player.getPersistentDataContainer().set(new NamespacedKey(plugin,"playerLap"), PersistentDataType.INTEGER,0);
